3ff2248a7e4bb93d432220381760b12ec26117b4
Your Name <your@example.com>
2016-05-06 10:41:08 -0400

    CylinderProgram_Initial

---
+++ Cylinder Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylinder_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cylinder Calculator 1.0");
+
+            Console.WriteLine("Enter the Cylinder's Radius: ");
+            string radiusAsAString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusAsAString);
+
+            Console.WriteLine("Enter the Cylinder's Height: ");
+            string heightAsAString = Console.ReadLine();
+            double height = Convert.ToDouble(heightAsAString);
+
+            double pi = 3.141592654;
+            double volume = pi * radius * height;
+            double surfaceArea = 2 * pi * radius * (radius * height);
+
+            Console.WriteLine("The Cylinder's volume is: " + volume + " cubic units. ");
+            Console.WriteLine("The Cylinder's surface area is:" + surfaceArea + " square units");
+
+            Console.ReadKey(); 
+                
+        }
+    }
+}
